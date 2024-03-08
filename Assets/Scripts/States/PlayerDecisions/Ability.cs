4ab2bdcdb5cbabcb734abe291121e22ac1e52f31
a22oscar <119612397+a22oscar@users.noreply.github.com>
2024-03-08 19:08:20 +0100

    recortes necesarios

--- Assets/Scripts/States/PlayerDecisions/Ability.cs
+++ Assets/Scripts/States/PlayerDecisions/Ability.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Ability : States
+public abstract class Ability 
 {
     [SerializeField] public float abilityRange;
-    protected Ability(GameObject stateGameObject) : base(stateGameObject)
-    {
-    }
+   
 
 }

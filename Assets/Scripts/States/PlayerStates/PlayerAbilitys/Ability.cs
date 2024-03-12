7c3ee4e806c6224f3e4e1f3abbe74671459bf2cd
a22oscar <119612397+a22oscar@users.noreply.github.com>
2024-03-12 19:17:01 +0100

    conectar el onabilitytime entre el objeto habilidad jiji komanche y el eslot estado

--- Assets/Scripts/States/PlayerStates/PlayerAbilitys/Ability.cs
+++ Assets/Scripts/States/PlayerStates/PlayerAbilitys/Ability.cs
@@ -7,7 +7,7 @@ public abstract class Ability : ScriptableObject{
     [SerializeField] protected float abilityBaseDamage;
     [SerializeField] protected float abilityScaling;
     [SerializeField] protected float abilityCD;
-    [SerializeField] protected float onststetime;
+    [SerializeField] public float onststetime;
 
     public abstract void OnEnterState(GameObject stateGameObject);
     public abstract void AbilityUpdate();

e75cb644df7572d6a143e79415f2e82949a6984f
a22oscar <119612397+a22oscar@users.noreply.github.com>
2024-03-13 18:34:34 +0100

    boicot al euro habilidad creada

--- Assets/Scripts/States/PlayerStates/PlayerAbilitys/AreaDamageAbility.cs
+++ Assets/Scripts/States/PlayerStates/PlayerAbilitys/AreaDamageAbility.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 [CreateAssetMenu(menuName = "Abilitys/AreaDamageAbility")]
 public class AreaDamageAbility : Ability
 {
+
+    private Transform playerTransform;
+    [SerializeField] LayerMask enemyLayerMask;
     public override void OnEnterState(GameObject stateGameObject)
     {
-        
+        playerTransform = stateGameObject.GetComponent<Transform>();
+        AreaDamage();
+        Debug.Log("nikinikole si o no?");
     }
 
     public override void OnExitState()
@@ -19,5 +25,20 @@ public class AreaDamageAbility : Ability
     public override void AbilityUpdate()
     {
         
+    }
+
+    private void AreaDamage()
+    {
+       
+        Collider[] hitColliders = Physics.OverlapSphere(playerTransform.position, abilityRange / 2, enemyLayerMask, QueryTriggerInteraction.UseGlobal);
+
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if (hitCollider.TryGetComponent<HealthBehaviour>(out HealthBehaviour healthBehaviour))
+            {
+                healthBehaviour.Damage(abilityBaseDamage);
+            }
+            Debug.Log("Impacto con: " + hitCollider.gameObject.name);
+        }
     }
 }

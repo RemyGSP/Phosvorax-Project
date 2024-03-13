744ce1d6e32ec69781b837109f853fedc04f1819
racso574 <96116063+racso574@users.noreply.github.com>
2024-03-13 01:35:05 +0100

    recortando cosasa irel

--- Assets/Scripts/States/PlayerDecisions/Ability2Decision.cs
+++ Assets/Scripts/States/PlayerDecisions/Ability2Decision.cs
@@ -8,7 +8,7 @@ public class Ability2Decision : Decision
     public override bool Decide(StateMachine stateMachine)
     {
         bool aux = false;
-        if (PlayerInputController.Instance.IsUsingAbility2() && PlayerTimers.Instance.abilityTimers[0] > PlayerTimers.Instance.abilityCD[0])
+        if (PlayerInputController.Instance.IsUsingAbility2() && PlayerTimers.Instance.abilityTimers[1] > PlayerTimers.Instance.abilityCD[1])
         {
             int currentAbility = PlayerInputController.Instance.GetCurrentAbility();
             if (currentAbility == 2)

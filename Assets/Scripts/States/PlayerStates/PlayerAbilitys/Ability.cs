93a842981f9275929b9da186ed384e3ced929034
a22oscar <119612397+a22oscar@users.noreply.github.com>
2024-03-13 17:09:29 +0100

    komanche jijiji

--- Assets/Scripts/States/PlayerStates/PlayerAbilitys/Ability.cs
+++ Assets/Scripts/States/PlayerStates/PlayerAbilitys/Ability.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Ability : ScriptableObject{
-    //[SerializeField] protected float abilityRange;
-    //[SerializeField] protected float abilityBaseDamage;
-    //[SerializeField] protected float abilityScaling;
+    [SerializeField] protected float abilityRange;
+    [SerializeField] protected float abilityBaseDamage;
+    [SerializeField] protected float abilityScaling;
     [SerializeField] protected float abilityCD;
     [SerializeField] public float onststetime;
 

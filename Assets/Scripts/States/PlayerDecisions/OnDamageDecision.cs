169290fb9b53b47b39c1eb1de47fd8eb8880dab6
a22oscar <119612397+a22oscar@users.noreply.github.com>
2024-03-19 16:00:28 +0100

    no se como se programan las decisiones aaaaaaaaaaaa

--- Assets/Scripts/States/PlayerDecisions/OnDamageDecision.cs
+++ Assets/Scripts/States/PlayerDecisions/OnDamageDecision.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "PlayerDecisions/OnDamageDecision")]
 public class OnDamageDecision : Decision
 {
-    // Start is called before the first frame update
-    void Start()
+    public override bool Decide(StateMachine stateMachine)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return stateMachine.gameObject.GetComponent<HealthBehaviour>().CheckIfDeath();
     }
 }

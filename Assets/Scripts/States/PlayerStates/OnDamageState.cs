966688ebdd6d0c731786180c5bb4c83879ad939b
a22oscar <119612397+a22oscar@users.noreply.github.com>
2024-03-19 19:28:55 +0100

    parpadea pero no se controla muybien la cantidad de parpadeo y ademas no se echa pa atras pero como los bichos de por si ya te echan para atras no se un poco rraro sabes

--- Assets/Scripts/States/PlayerStates/OnDamageState.cs
+++ Assets/Scripts/States/PlayerStates/OnDamageState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,24 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "States/PlayerOnDamageState")]
 public class OnDamageState : States
 {
+    [SerializeField] private float invulnerabilityTime;
+    public AnimationCurve blinkCurve; // Curva de animación para el parpadeo
+    private bool isBlinking = false;
+    private HealthBehaviour playerhealth;
+
     public OnDamageState(GameObject stateGameObject) : base(stateGameObject)
     {
     }
+    
     public override void Start()
     {
-        //Debug.Log("medañolmao");
+        playerhealth = stateGameObject.GetComponent<HealthBehaviour>();
+        MonoInstance.instance.StartCoroutine(InvulnerabilityCorutine());
+    }
+
+    public override void FixedUpdate()
+    {
+   
     }
 
     public override void OnExitState()
@@ -22,4 +35,27 @@ public class OnDamageState : States
     {
         
     }
+
+    IEnumerator InvulnerabilityCorutine()
+    {
+        isBlinking = true;
+        float timer = 0f;
+        playerhealth.SetDamageModifier(0);
+        while (timer < invulnerabilityTime)
+        {
+            float blinkValue = blinkCurve.Evaluate(timer / invulnerabilityTime);
+
+            // Cambia la visibilidad del jugador
+            PlayerReferences.instance.playerRenderer.enabled = !PlayerReferences.instance.playerRenderer.enabled;
+
+            yield return null;
+            timer += Time.deltaTime;
+
+        }
+
+        PlayerReferences.instance.playerRenderer.enabled = true; // Asegúrate de que el jugador esté visible al final del parpadeo
+        playerhealth.SetDamageModifier(1);
+        isBlinking = false;
+    }
+
 }

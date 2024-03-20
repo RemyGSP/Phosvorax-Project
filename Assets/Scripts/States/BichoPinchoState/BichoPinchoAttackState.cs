9d1ce4b307ec59d9bae631a5cd3635fd23a30fd2
RemyGSP <guillem0405@gmail.com>
2024-03-20 13:32:29 +0100

    BichoPinchoFinished

--- Assets/Scripts/States/BichoPinchoState/BichoPinchoAttackState.cs
+++ Assets/Scripts/States/BichoPinchoState/BichoPinchoAttackState.cs
@@ -38,6 +38,7 @@ public class BichoPinchoAttackState : States
 
     public override void Start()
     {
+        GameObject feedbackSpawnPos = stateGameObject.GetComponent<FeedbackPosition>().GetFeedbackGameObject();
         infoContainer = stateGameObject.GetComponent<BichoPinchoReferences>();
         infoContainer.StopAttackTimer();
         infoContainer.RestartAttackTimer();
@@ -47,14 +48,14 @@ public class BichoPinchoAttackState : States
         targetPosition = PlayerReferences.instance.GetPlayerCoordinates();
         targetPosition.y = stateGameObject.transform.position.y;
         stateGameObject.transform.LookAt(targetPosition);
-        Instantiate(feedback, stateGameObject.GetComponent<FeedbackPosition>().GetFeedbackSpawnPos(),Quaternion.identity);
-
+        // Instantiate the object
+        GameObject instantiatedObject = Instantiate(feedback, feedbackSpawnPos.transform.position, Quaternion.identity);
+        instantiatedObject.GetComponent<FollowPlayer>().SetPlayer(feedbackSpawnPos);
     }
 
     public override void Update()
     {
         currentTime += Time.deltaTime;
-
         float acceleration = accelerationCurve.Evaluate(currentTime);
         if (reached)
         {
@@ -68,15 +69,19 @@ public class BichoPinchoAttackState : States
         }
         else
         {
-            stateGameObject.transform.LookAt(targetPosition);
             rb.velocity = (targetPosition - stateGameObject.transform.position).normalized * force * acceleration;
-
+            //Hace falta el Lookat aqui porque sino se rompe la rotacion y hago este if porque si se ejecuta todo el rato que esta acelerando a veces hace el lookAt una vez se ha pasado de la 
+            //direccion y corrige su direccion hacia atras
+            if (currentTime > 0.0f && currentTime < 0.7f)
+            {
+                stateGameObject.transform.LookAt(targetPosition);
+            }
         }
         if (Vector3.Distance(stateGameObject.transform.position, targetPosition) < 0.5f)
         {
             reached = true;
         }
-        Debug.Log("Acceleration: " + acceleration + " Deceleration: " + deccelerationCurve.Evaluate(stopTimer));
+
     }
 
     public override void OnTriggerEnter(Collider other)
@@ -86,7 +91,6 @@ public class BichoPinchoAttackState : States
             other.gameObject.GetComponent<HealthBehaviour>().Damage(infoContainer.GetDamage());
         }
     }
-
 
 
 }

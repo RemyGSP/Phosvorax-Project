17431e1cdf6310185dfce2df6874bbe707aa1385
A22Marti <146969715+A22Marti@users.noreply.github.com>
2024-03-20 20:11:42 +0100

    shader and others

---
+++ Assets/Scripts/States/Ranged Enemy/RangedEnemyAttackState.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[CreateAssetMenu(menuName = "EnemyStates/EnemyMeleeAttackState")]
+public class RangedEnemyAttackState : States
+{
+
+    #region Constructor
+    public RangedEnemyAttackState(GameObject stateGameObject) : base(stateGameObject)
+    {
+    }
+    #endregion
+
+
+    #region Variables
+    [SerializeField] LayerMask playerLayerMask;
+
+    private RotateCharacter rotateCharacter;
+    private Rigidbody rigidBody;
+    private Animator anim;
+    [SerializeField] private float attackDelay = 0.5f; // Tiempo de retraso antes de ejecutar el ataque
+    [SerializeField] private float attackOffset = 1.0f; // Distancia desde el jugador para el inicio del ataque
+    [SerializeField] private float sphereSize; // Tamaño del área de detección
+    [SerializeField] private float attackDamage;
+
+    //controlar que el daño vaya con la animacion
+    float animationLength;
+    float currentAttackTime;
+    bool canAttack;
+    private float currentAttackDelay;
+    [Header("Timers")]
+    public float AttackTimer;
+    public float AttackCD;
+
+    [SerializeField] private float maxAttackDistance = 5f;
+
+    [SerializeField] private AttackAreaVisualizer attackAreaVisualizer;
+
+    private NavMeshAgent enemy;
+
+    #endregion
+
+    #region AbstractMethods
+    public override States CheckTransitions()
+    {
+        bool notChanged = true;
+        int counter = 0;
+        States newEnemyState = null;
+
+        while (notChanged)
+        {
+            newEnemyState = stateTransitions[counter].GetExitState(stateGameObject.GetComponent<StateMachine>());
+            if (newEnemyState != null)
+            {
+                notChanged = false;
+                newEnemyState = Instantiate(newEnemyState);
+                newEnemyState.InitializeState(stateGameObject);
+                newEnemyState.Start();
+            }
+            if (counter < stateTransitions.Length - 1)
+            {
+                counter++;
+            }
+            else
+            {
+                notChanged = false;
+            }
+        }
+
+        return newEnemyState;
+    }
+
+    public override void InitializeState(GameObject gameObject)
+    {
+        stateGameObject = gameObject;
+    }
+    #endregion
+
+    #region Methods
+
+    public override void Start()
+    {
+        enemy = stateGameObject.GetComponent<NavMeshAgent>();
+        enemy.speed = 0;
+        rotateCharacter = stateGameObject.GetComponent<RotateCharacter>();
+        anim = stateGameObject.GetComponent<Animator>();
+        rigidBody = stateGameObject.GetComponent<Rigidbody>();
+        currentAttackDelay = attackDelay;
+
+        ExecuteAnim();
+    }
+
+    private void ExecuteAnim()
+    {
+        //anim.SetTrigger("attack");
+        currentAttackTime = 0;
+        //animationLength = anim.GetCurrentAnimatorClipInfo(0).Length;
+        stateGameObject.transform.rotation = rotateCharacter.NonSmoothenedRotation(PlayerReferences.instance.GetPlayerCoordinates() - stateGameObject.transform.position);
+        currentAttackDelay = attackDelay;
+        canAttack = true;
+
+    }
+
+    void ExecuteAttack()
+    {
+
+
+        Vector3 attackDirection = stateGameObject.transform.forward;
+
+        // Lanza un rayo en la dirección del ataque
+        RaycastHit hit;
+        if (Physics.Raycast(stateGameObject.transform.position, attackDirection, out hit, maxAttackDistance, playerLayerMask))
+        {
+            // Si el rayo golpea un objeto en la capa del jugador, aplica daño
+            if (hit.collider.TryGetComponent<HealthBehaviour>(out HealthBehaviour healthBehaviour))
+            {
+                healthBehaviour.Damage(attackDamage);
+            }
+
+            Debug.Log("Impacto con: " + hit.collider.gameObject.name);
+        }
+
+        canAttack = false;
+    }
+
+
+    public override void FixedUpdate()
+    {
+        currentAttackDelay -= Time.deltaTime;
+        currentAttackTime += Time.deltaTime;
+
+        if (currentAttackDelay <= 0)
+        {
+            if (canAttack)
+            {
+                ExecuteAttack();
+                PlayerTimers.Instance.playerBasicAttackTimer = 0;
+            }
+            currentAttackDelay = attackDelay;
+            canAttack = true;
+        }
+    }
+    public override void Update()
+    {
+        stateGameObject.transform.rotation = rotateCharacter.Rotate(stateGameObject.transform.rotation, PlayerReferences.instance.GetPlayerCoordinates() - stateGameObject.transform.position, 0.5f);
+        AttackTimer += Time.deltaTime;
+    }
+
+    public override void OnExitState()
+    {
+        return;
+    }
+
+    #endregion
+}
+

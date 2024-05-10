1d5735cebab1af5fcac0d12ca29ed5a7617cfd91
Oscar Revilla <96116063+racso574@users.noreply.github.com>
2024-05-10 20:26:54 +0200

    E

---
+++ Assets/Scripts/CloseDoorWhenCrossed.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoorWhenCrossed : MonoBehaviour
+{
+    [SerializeField] private GameObject bridge;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
